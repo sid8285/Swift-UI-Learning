1d675b66882586c50af6eb6921b0bd352c795218
Siddhant Srivastava <sid.srivastava@outlook.com>
2025-07-22 00:40:25 -0400

    quick git repo fix

--- toDoList/ContentView.swift
+++ toDoList/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello,this is Sid!")
+            Text("Hello World!")
         }
         .padding()
     }
